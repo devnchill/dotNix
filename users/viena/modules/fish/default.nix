bff59295628aecedca9a96b74e9fbfac2f04c7c5
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-05 14:17:56 +0530

    starship and vi mode for fish
    fzf and zoxide integration in fish

--- users/viena/modules/fish/default.nix
+++ users/viena/modules/fish/default.nix
@@ -1,14 +1,47 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
   programs.fish = {
     enable = true;
-    plugins = [ { name = "grc"; src = pkgs.fishPlugins.grc.src; } ];
+
+    interactiveShellInit = ''
+      set -g fish_greeting
+      fish_vi_key_bindings
+    '';
+
+    plugins = [
+      { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+    ];
   };
 
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  	settings={
+		battery.disabled=false;
+		character = {
+		  success_symbol = "[λ](bold green)";
+		  error_symbol = "[λ](bold red)";
+	  	  vimcmd_symbol = "[λ](bold purple)";
+	  	  vimcmd_visual_symbol = "[λ](bold blue)";
+	  	  vimcmd_replace_one_symbol = "[λ](bold yellow)";
+	  	  vimcmd_replace_symbol = "[λ](bold pink)";
+      };
+	};
+  };
 
- home.packages = with pkgs;[
- 	grc
- ];
+  programs.fzf = {
+  	enable = true;
+	enableFishIntegration = true;
+  };
+
+  programs.zoxide = {
+	enable  = true;
+	enableFishIntegration = true;
+  };
+
+  home.packages = with pkgs; [
+    grc
+  ];
 }
 
