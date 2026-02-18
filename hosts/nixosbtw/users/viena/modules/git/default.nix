4f45a9e6733b43c5e778bd6617ed3f219127dd60
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-18 13:32:51 +0530

    add git-crypt and configure git to use gpg to sign commits
    Configure git-crypt for secrets
    
    Add 1 git-crypt collaborator
    
    New collaborators:
    
        BF798CD0CA25210699E1ED9EF826F032DDDA5560
            viena (gpg for git ssh) <169875752+devnchill@users.noreply.github.com>
    
    Add SSH and GPG keys with git-crypt encryption
    
    add chat gpg key

--- hosts/nixosbtw/users/viena/modules/git/default.nix
+++ hosts/nixosbtw/users/viena/modules/git/default.nix
@@ -1,16 +1,19 @@
-{config,pkgs,...}:
+{ config, pkgs, ... }:
 
 {
   programs.git = {
-      enable = true;
-      settings = {
-      user={
-          name="Viena";
-          email = "169875752+devnchill@users.noreply.github.com";
+    enable = true;
+    settings = {
+      user = {
+        name = "Viena";
+        email = "169875752+devnchill@users.noreply.github.com";
       };
-      core.editor="nvim";
-      init.defaultbranch="main";
-      pull.rebase=false;
-      };
+      core.editor = "nvim";
+      init.defaultbranch = "main";
+      pull.rebase = false;
+      commit.gpgsign = true;
+      user.signingkey = "F826F032DDDA5560";
+      gpg.program = "${pkgs.gnupg}/bin/gpg";
+    };
   };
 }
