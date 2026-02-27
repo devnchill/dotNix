cf73b8a810f5e08a197c035519d1f49f6a4dcdb8
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-27 18:22:26 +0530

    os : install postgres

--- hosts/nixosbtw/modules/services/default.nix
+++ hosts/nixosbtw/modules/services/default.nix
@@ -24,4 +24,15 @@
     mode = "0644";
     text = "nameserver ::1";
   };
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "defaultdb" ];
+    package = pkgs.postgresql;
+    authentication = pkgs.lib.mkOverride 10 ''
+            		  #type database  DBuser  auth-method
+            		  local all       all     trust
+      			  host    all    all    127.0.0.1/32 md5
+            	'';
+  };
 }
