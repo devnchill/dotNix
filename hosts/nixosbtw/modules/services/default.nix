a39aea4d82cb5de8074ed9b7322b2ccd77666fca
Viena <169875752+devnchill@users.noreply.github.com>
2026-03-04 08:53:38 +0530

    feat: enable psql via ipv6/4 and reduce mouse scroll factor

--- hosts/nixosbtw/modules/services/default.nix
+++ hosts/nixosbtw/modules/services/default.nix
@@ -30,9 +30,10 @@
     ensureDatabases = [ "defaultdb" ];
     package = pkgs.postgresql;
     authentication = pkgs.lib.mkOverride 10 ''
-            		  #type database  DBuser  auth-method
-            		  local all       all     trust
-      			  host    all    all    127.0.0.1/32 md5
-            	'';
+                     		  #type database  DBuser  auth-method
+                     		  local all       all     trust
+               			  host    all    all    127.0.0.1/32 md5
+      	 					  host    all       all   ::1/128    md5
+                     	'';
   };
 }
