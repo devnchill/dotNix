83e6b0bffcc38a2e1c02d5e3a38b88f5439fe1f4
Viena <169875752+devnchill@users.noreply.github.com>
2026-03-22 15:17:43 +0530

    nvim : add compile and run function for cpp

---
+++ hosts/nixosbtw/users/viena/config/nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,28 @@
+vim.keymap.set("n", "<F5>", function()
+	vim.fn.mkdir("/tmp/dsa_binaries", "p")
+
+	local file = vim.fn.expand("%:p")
+	local output = "/tmp/dsa_binaries/" .. vim.fn.expand("%:t:r")
+
+	vim.opt_local.makeprg = "g++ -std=c++17 -Wall -Wextra -o " .. output .. " " .. file
+
+	vim.cmd("compiler gcc")
+	vim.cmd("silent make | redraw!")
+
+	local qf = vim.fn.getqflist()
+	if #qf > 0 then
+		vim.cmd("copen")
+		vim.cmd("cc")
+		return
+	end
+
+	vim.cmd("cclose")
+
+	print("Compiled successfully")
+
+	if vim.fn.executable(output) == 1 then
+		vim.cmd("terminal " .. output)
+	else
+		print("Binary not found")
+	end
+end, { buffer = true })
