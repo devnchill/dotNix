b11be4eb3149efe09756d56767431b01bd2d0832
Viena <169875752+devnchill@users.noreply.github.com>
2026-04-07 12:36:50 +0530

    chore: add my personal cache

---
+++ hosts/nixosbtw/modules/cachix/default.nix
@@ -0,0 +1,17 @@
+{ lib, config, ... }:
+
+let
+  devnchillKey = "devnchill.cachix.org-1:TilbmG02gm6LclTLYflIWNxmOXKZLMf6E/Os9DxAjHc=";
+in
+{
+  nix = {
+    settings.substituters = [
+      "https://devnchill.cachix.org"
+      "https://cache.nixos.org"
+    ];
+
+    settings.trusted-public-keys = [
+      devnchillKey
+    ];
+  };
+}
