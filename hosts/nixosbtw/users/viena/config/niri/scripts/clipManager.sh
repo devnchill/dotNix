01c79b8a8e622a56450ceb20e5a3e3ea5dc3371e
Viena <169875752+devnchill@users.noreply.github.com>
2026-04-16 09:07:07 +0530

    niri : add script to RD clipboard and keybind to execute it

---
+++ hosts/nixosbtw/users/viena/config/niri/scripts/clipManager.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Clipboard Manager. This script uses cliphist, rofi, and wl-copy.
+
+# Variables
+msg='CTRL DEL = cliphist del (entry)   or   ALT DEL - cliphist wipe (all)'
+# Actions:
+# CTRL Del to delete an entry
+# ALT Del to wipe clipboard contents
+
+# Check if rofi is already running
+if pidof rofi > /dev/null; then
+  pkill rofi
+fi
+
+while true; do
+    result=$(
+        rofi -i -dmenu \
+            -kb-custom-1 "Control-Delete" \
+            -kb-custom-2 "Alt-Delete" \
+			-mesg "$msg" \
+            < <(cliphist list) 
+    )
+
+    case "$?" in
+        1)
+            exit
+            ;;
+        0)
+            case "$result" in
+                "")
+                    continue
+                    ;;
+                *)
+                    cliphist decode <<<"$result" | wl-copy
+                    exit
+                    ;;
+            esac
+            ;;
+        10)
+            cliphist delete <<<"$result"
+            ;;
+        11)
+            cliphist wipe
+            ;;
+    esac
+done
+
