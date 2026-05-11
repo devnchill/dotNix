29c44a46aa942e5049ec8a0291e1769f64818c6f
Viena <169875752+devnchill@users.noreply.github.com>
2026-05-11 18:00:59 +0530

    feat(tmux) : exclude useless dirs for sessions

--- hosts/nixosbtw/users/viena/modules/scripts/bin/tmux-sessionizer.sh
+++ hosts/nixosbtw/users/viena/modules/scripts/bin/tmux-sessionizer.sh
@@ -1,34 +1,52 @@
 #!/usr/bin/env bash
 
-# Custom project roots
 SEARCH_DIRS=(
   "$HOME/dev/"
 )
 
-# Select a directory via fzf
+EXCLUDES=(
+  .git
+  node_modules
+  .venv
+  venv
+  __pycache__
+  .npm
+  .cache
+  dist
+  build
+)
+
+find_excludes=()
+
+for dir in "${EXCLUDES[@]}"; do
+  find_excludes+=( -name "$dir" -o )
+done
+
+unset 'find_excludes[-1]' # remove trailing -o
+
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  selected=$(find "${SEARCH_DIRS[@]}" -mindepth 1 -type d | fzf)
+  selected=$(
+    find "${SEARCH_DIRS[@]}" \
+      \( "${find_excludes[@]}" \) -prune -o \
+      -mindepth 1 -type d -print |
+      fzf
+  )
 fi
 
-# Exit if no selection
 [[ -z $selected ]] && exit 0
 
-# Clean name for tmux session
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-# Start tmux if not running
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
   tmux new-session -s "$selected_name" -c "$selected"
   exit 0
 fi
 
-# Create session if it doesn't exist
 if ! tmux has-session -t="$selected_name" 2>/dev/null; then
   tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-# Switch to it
 tmux switch-client -t "$selected_name"
