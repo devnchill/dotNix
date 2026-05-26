fb3279fa18061f15094998d9e6eaf92360a09081
Viena <169875752+devnchill@users.noreply.github.com>
2026-05-26 18:19:17 +0530

    refactor: move config out of host so that it could be shared by multiple host

--- hosts/kalki/users/viena/home.nix
+++ hosts/kalki/users/viena/home.nix
@@ -22,11 +22,11 @@
     stateVersion = "25.11";
 
     file = {
-      ".config/kitty".source = ./config/kitty;
-      ".config/niri".source = ./config/niri;
-      ".config/waybar".source = ./config/waybar;
-      ".config/nvim".source = ./config/nvim;
-      ".config/fastfetch".source = ./config/fastfetch;
+      ".config/kitty".source = ../../../../config/kitty;
+      ".config/niri".source = ../../../../config/niri;
+      ".config/waybar".source = ../../../../config/waybar;
+      ".config/nvim".source = ../../../../config/nvim;
+      ".config/fastfetch".source = ../../../../config/fastfetch;
     };
   };
 }
