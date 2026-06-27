719a4488feef28b36c055da04add42b5063c9fa5
Viena <169875752+devnchill@users.noreply.github.com>
2026-06-27 14:43:45 +0530

    add lazygit config

--- hosts/kalki/users/viena/home.nix
+++ hosts/kalki/users/viena/home.nix
@@ -26,6 +26,7 @@
       ".config/niri".source = ../../../../config/niri;
       ".config/waybar".source = ../../../../config/waybar;
       ".config/nvim".source = ../../../../config/nvim;
+      ".config/lazygit".source = ../../../../config/lazygit;
       ".config/fastfetch".source = ../../../../config/fastfetch;
     };
   };
