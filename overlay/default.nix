0dc788f51e46b376faa7be8449cf5af4e1710479
Viena <169875752+devnchill@users.noreply.github.com>
2026-07-16 13:59:35 +0530

    fix(spicetify): add temporary overlay for spicetify to avoid rendeirng issue
    Signed-off-by: Viena <169875752+devnchill@users.noreply.github.com>

--- overlay/default.nix
+++ overlay/default.nix
@@ -2,4 +2,21 @@ _: prev: {
   mpv = prev.mpv.override {
     youtubeSupport = false;
   };
+
+  spicetify-cli = prev.spicetify-cli.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+      prev.nodejs
+      prev.esbuild
+    ];
+    postBuild = ''
+      esbuild ./src/jsHelper/spicetifyWrapper/index.js \
+        --bundle --minify --target=chrome108 --format=iife \
+        --outfile=spicetifyWrapper.js
+    '';
+    postInstall = (old.postInstall or "") + ''
+      chmod -R u+w $out/share/spicetify/jsHelper
+      cp spicetifyWrapper.js $out/share/spicetify/jsHelper/spicetifyWrapper.js
+    '';
+  });
+
 }
